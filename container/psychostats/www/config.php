4a0ca4b685afb8784ea3d58d6a7671babbe69e1a
Ed Anderson <36516131+eca20@users.noreply.github.com>
2026-01-12 02:05:25 -0500

    updates from ai

--- container/psychostats/www/config.php
+++ container/psychostats/www/config.php
@@ -1,10 +1,10 @@
 <?php
 $dbtype = 'mysql';
-$dbhost = 'localhost';
-$dbport = '';
-$dbname = 'psychostats3_1';
-$dbuser = 'ps3';
-$dbpass = '';
+$dbhost = getenv('PS_DB_HOST') ?: 'psychostats_db';
+$dbport = getenv('PS_DB_PORT') ?: '3306';
+$dbname = getenv('PS_DB_NAME') ?: 'psychostats3_1';
+$dbuser = getenv('PS_DB_USER') ?: 'ps3';
+$dbpass = getenv('PS_DB_PASSWORD') ?: 'ps3pass';
 $dbtblprefix = 'ps_';
 $site_url = '';
 ?>

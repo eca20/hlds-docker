bc62e3dcd162702c79f0b9bea9694cea61869813
Ed Anderson <36516131+eca20@users.noreply.github.com>
2026-01-12 00:56:03 -0500

    edit dockerfile and entrypoint.sh

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,47 +1,60 @@
 #!/usr/bin/env sh
 
+set -eu
+
 GAME=${GAME:-valve}
 VERSION=${VERSION:-custom}
 IMAGE=${IMAGE:-custom}
 
-if echo "$@" | grep -qv "+map"; then
-  echo -e "\e[33mWarning: No +map specified in the command. Server will start but may not be joinable.\e[0m"
-fi
+# Warn if no +map was provided in the args.
+# (Use POSIX-safe printf instead of echo -e.)
+case " $* " in
+  *" +map "*) : ;;
+  *) printf '\033[33mWarning: No +map specified in the command. Server will start but may not be joinable.\033[0m\n' ;;
+esac
+
+# Ensure game directory exists (helps first-run + avoids rsync target issues)
+mkdir -p "/opt/steam/hlds/$GAME"
 
 # Push mods and config files from their temp directories to the server directories.
-if [ -d /temp/mods ]
-then
-  rsync --recursive --update --chown=steam:steam /temp/mods/* /opt/steam/hlds
+# IMPORTANT: Do NOT use rsync --chown here; the container typically runs as user 'steam'
+# and --chown will fail unless running as root.
+
+if [ -d /temp/mods ]; then
+  # Only sync if there's something to copy
+  if [ "$(ls -A /temp/mods 2>/dev/null || true)" ]; then
+    rsync -r --update /temp/mods/ /opt/steam/hlds/
+  fi
 fi
 
-if [ -d /temp/config ]
-then
-  rsync --recursive --update --chown=steam:steam /temp/config/* /opt/steam/hlds/$GAME
+if [ -d /temp/config ]; then
+  if [ "$(ls -A /temp/config 2>/dev/null || true)" ]; then
+    rsync -r --update /temp/config/ "/opt/steam/hlds/$GAME/"
+  fi
 fi
 
+cat <<EOF
+                          ..::::::..
+                      :-=++++++++++++=-:
+                  :=++++=--::...::-=++++=:
+                :=+++=:              :-++++:
+                =+++-     =====:         -+++=
+              ++++.      ===+++.         .=+++
+              =+++           :+++           =+++
+            :+++.           -+++=          .+++:
+            =++=           =+++++-          =++=
+            =++-         .=++-:+++:         -+++
+            =++=        .+++-  -+++.        =++=
+            :+++.      :+++.    =+++       .+++:
+              =+++     =++=.      ++++++=   =++=
+              =+++.  -==-        .+++=-: .=+++
+                =+++-.                   -+++=
+                :=+++=:              :=+++=:
+                  :=+++++=-::..::-=+++++=:
+                      :-=++++++++++++=-:
+                          ..::::::..
 
-echo "
-                          ..::::::..              
-                      :-=++++++++++++=-:          
-                  :=++++=--::...::-=++++=:       
-                :=+++=:              :-++++:     
-                =+++-     =====:         -+++=    
-              ++++.      ===+++.         .=+++   
-              =+++           :+++           =+++  
-            :+++.           -+++=          .+++: 
-            =++=           =+++++-          =++= 
-            =++-         .=++-:+++:         -+++ 
-            =++=        .+++-  -+++.        =++= 
-            :+++.      :+++.    =+++       .+++: 
-              =+++     =++=.      ++++++=   =++=  
-              =+++.  -==-        .+++=-: .=+++   
-                =+++-.                   -+++=    
-                :=+++=:              :=+++=:     
-                  :=+++++=-::..::-=+++++=:       
-                      :-=++++++++++++=-:          
-                          ..::::::..              
-
-                          hlds-docker 
+                          hlds-docker
 
 ====================================================================
 💿 Image: $IMAGE
@@ -56,9 +69,10 @@ echo "
 █  🔔 Feedback / Issues: https://github.com/JamesIves/hlds-docker  █
 █                                                                  █
 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-"
+EOF
 
-echo "\e[32mStarting Half-Life Dedicated Server...\e[0m"
+printf '\033[32mStarting Half-Life Dedicated Server...\033[0m\n'
 
 # Start the server with the specified game and any additional arguments.
-./hlds_run "-game $GAME $@"
+# NOTE: Do not wrap args in a single quoted string; pass them as real argv entries.
+exec ./hlds_run -game "$GAME" "$@"
